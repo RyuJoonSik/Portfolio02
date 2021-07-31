b8f3057dcb1387d5e504179cde255369aaafbfc6
RyuJoonSik <71337000+RyuJoonSik@users.noreply.github.com>
2021-08-01 02:13:56 +0900

    Portfolio02

--- js/main.js
+++ js/main.js
@@ -16,39 +16,51 @@ DASHBOARD_TEMP.drawMeter();
 DASHBOARD_HUM.drawMeter();
 
 setInterval(() => {
-  // DASHBOARD_TEMP.setValue(Number(TEMP));
-  // DASHBOARD_HUM.setValue(Number(HUM));
-  // DASHBOARD_TEMP.drawMeter();
-  // DASHBOARD_HUM.drawMeter();
-  // TEMP_VAL.textContent = TEMP + "℃";
-  // HUM_VAL.textContent = HUM + "%";
-  // const [YEAR, MONTH, DAY, DOW] = TIME.split(" ")[0].split("-");
-  // const [HOUR, MINUTE, SECOND] = TIME.split(" ")[1].split(":");
-  // let dayOfWeek = "";
-  // switch (DOW * 1) {
-  //   case 0:
-  //     dayOfWeek = "일";
-  //     break;
-  //   case 1:
-  //     dayOfWeek = "월";
-  //     break;
-  //   case 2:
-  //     dayOfWeek = "화";
-  //     break;
-  //   case 3:
-  //     dayOfWeek = "수";
-  //     break;
-  //   case 4:
-  //     dayOfWeek = "목";
-  //     break;
-  //   case 5:
-  //     dayOfWeek = "금";
-  //     break;
-  //   case 6:
-  //     dayOfWeek = "토";
-  //     break;
-  // }
-  // CUR_TIME.textContent = `${YEAR}년 ${MONTH}월 ${DAY}일 ${dayOfWeek}요일 ${HOUR}시 ${MINUTE}분 ${SECOND}초`;
+  const TEMP = (Math.random() * 10 + 25).toFixed(2);
+  const HUM = (((Math.random() * 10) / 6) * 10 + 50).toFixed(2);
+
+  DASHBOARD_TEMP.setValue(TEMP);
+  DASHBOARD_HUM.setValue(HUM);
+  DASHBOARD_TEMP.drawMeter();
+  DASHBOARD_HUM.drawMeter();
+  TEMP_VAL.textContent = TEMP + "℃";
+  HUM_VAL.textContent = HUM + "%";
+
+  const DATE = new Date();
+  const YEAR = DATE.getFullYear();
+  const MONTH = DATE.getMonth() + 1;
+  const DAY = DATE.getDate();
+  const HOUR = DATE.getHours();
+  const MINUTE = DATE.getMinutes();
+  const SECOND = DATE.getSeconds();
+  let dayOfWeek = "";
+  switch (DATE.getDay()) {
+    case 0:
+      dayOfWeek = "일";
+      break;
+    case 1:
+      dayOfWeek = "월";
+      break;
+    case 2:
+      dayOfWeek = "화";
+      break;
+    case 3:
+      dayOfWeek = "수";
+      break;
+    case 4:
+      dayOfWeek = "목";
+      break;
+    case 5:
+      dayOfWeek = "금";
+      break;
+    case 6:
+      dayOfWeek = "토";
+      break;
+  }
+
+  console.log(MONTH);
+
+  CUR_TIME.textContent = `${YEAR}년 ${MONTH}월 ${DAY}일 (${dayOfWeek}) ${HOUR}시 ${MINUTE}분 ${SECOND}초`;
 }, 1000);
 
 window.addEventListener("resize", () => {
